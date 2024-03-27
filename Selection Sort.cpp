ff2448151fba6dc8adf0971d91128da8a1af2865
Mohammad Ashikur Rahman <122194673+Ashik2418@users.noreply.github.com>
2024-03-27 23:19:17 +0600

    Selection Sort.cpp

---
+++ Selection Sort.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+# define int long long
+#define opti ios_base::sync_with_stdio(false);
+# define mise cin.tie(NULL);
+#include <algorithm>
+
+void selectionSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; ++i) {
+        int minIndex = i;
+        for (int j = i + 1; j < size; ++j) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        swap(arr[i], arr[minIndex]);
+    }
+}
+
+signed main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Original array: ";
+    for (int num : arr) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    selectionSort(arr, size);
+
+    cout << "Sorted array: ";
+    for (int num : arr) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
